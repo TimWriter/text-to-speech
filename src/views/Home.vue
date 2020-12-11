a9c4d5fe6e009f1fe0d2d4f3e3e397a637a2a62e
Tim Schreiber <tim.schreiber@outlook.com>
2020-12-11 13:54:54 +0100

    text-to-speach preperation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TextToSpeach />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TextToSpeach from '@/components/TextToSpeach.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TextToSpeach,
   }
 }
 </script>
